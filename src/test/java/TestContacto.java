d2f9d94ead390c995b80d546c778f3027425e3c9
IgnacioParra19 <ignacioparraingaramo@gmail.com>
2023-10-04 20:42:37 -0300

    hice tests

--- src/test/java/TestContacto.java
+++ src/test/java/TestContacto.java
@@ -1,3 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.correotp.Contacto;
+
 public class TestContacto {
-    
+    @Test
+    public void testConstructorAndGetters() {
+        Contacto contacto = new Contacto("John Doe", "john@example.com");
+
+        assertNotNull(contacto);
+        assertEquals("John Doe", contacto.getNombre());
+        assertEquals("john@example.com", contacto.getEmail());
+    }
+
+    @Test
+    public void testSetters() {
+        Contacto contacto = new Contacto("John Doe", "john@example.com");
+
+        contacto.setNombre("Jane Smith");
+        contacto.setEmail("jane@example.com");
+
+        assertEquals("Jane Smith", contacto.getNombre());
+        assertEquals("jane@example.com", contacto.getEmail());
+    }
 }
--- src/test/java/TestEmail.java
+++ src/test/java/TestEmail.java
@@ -1,3 +1,45 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.correotp.Contacto;
+import com.correotp.Email;
+
 public class TestEmail {
-    
+        @Test
+    public void testConstructorAndGetters() {
+        Contacto remitente = new Contacto("John Doe", "john@example.com");
+        List<Contacto> para = new ArrayList<>();
+        para.add(new Contacto("Alice Smith", "alice@example.com"));
+        para.add(new Contacto("Bob Johnson", "bob@example.com"));
+        
+        Email email = new Email("Prueba", "Este es el contenido de prueba", remitente, para);
+
+        assertNotNull(email);
+        assertEquals("Prueba", email.getAsunto());
+        assertEquals("Este es el contenido de prueba", email.getContenido());
+        assertEquals(remitente, email.getRemitente());
+        assertEquals(para, email.getPara());
+    }
+@Test
+    public void testSetters() {
+        Email email = new Email("Prueba", "Este es el contenido de prueba", null, null);
+
+        email.setAsunto("Nuevo asunto");
+        email.setContenido("Nuevo contenido");
+        Contacto remitente = new Contacto("John Doe", "john@example.com");
+        email.setRemitente(remitente);
+        List<Contacto> para = new ArrayList<>();
+        para.add(new Contacto("Alice Smith", "alice@example.com"));
+        para.add(new Contacto("Bob Johnson", "bob@example.com"));
+        email.setPara(para);
+
+        assertEquals("Nuevo asunto", email.getAsunto());
+        assertEquals("Nuevo contenido", email.getContenido());
+        assertEquals(remitente, email.getRemitente());
+        assertEquals(para, email.getPara());
+    }
 }
